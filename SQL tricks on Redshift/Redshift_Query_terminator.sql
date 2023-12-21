ccabcd1165e3a3060b7869ac468a461806cac30a
Guruprasada Shridhar Hegde <85961223+GuruprasadaShridharHegde@users.noreply.github.com>
2023-12-21 16:30:16 +0530

    Update Redshift_Query_terminator.sql

--- SQL tricks on Redshift/Redshift_Query_terminator.sql
+++ SQL tricks on Redshift/Redshift_Query_terminator.sql
@@ -1,4 +1,4 @@
 -- Identify queries for specific users
 SELECT pg_terminate_backend(pg_stat_activity.procpid)
 FROM pg_stat_activity
-WHERE usename IN ('guruhegde','kinzashabbir','quicksight','jenkins');
+WHERE usename IN ('user_name1','user_name2','user_name3','user_name4');
